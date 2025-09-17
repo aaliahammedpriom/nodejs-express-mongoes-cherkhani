001f1014fe984d7b96bdd7c2bf66ecb58602bbf6
aaliahammedpriom <aaliahammedpriom66@gmail.com>
2025-09-17 18:55:52 +0600

    mongoose build in validator

--- module17/dist/model/user.model.js
+++ module17/dist/model/user.model.js
@@ -2,14 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const userSchema = new mongoose_1.Schema({
-    name: { type: String, required: true },
-    email: { type: String, trim: true, lowercase: true, unique: true, required: true },
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"]
+    },
     role: {
         type: String,
-        enum: ["student", "it", "teacher", "admin"],
+        enum: {
+            values: ["student", "it", "teacher", "admin"],
+            message: "{VALUE} is not a valid role"
+        },
         default: "student"
     },
-    isActive: { type: Boolean, required: true, default: true }
-});
+    isActive: {
+        type: Boolean,
+        required: [true, "Status is required"],
+        default: true
+    }
+}, { timestamps: true }); // optional: automatically adds createdAt and updatedAt
 const User = (0, mongoose_1.model)("User", userSchema);
 exports.default = User;
--- module17/src/model/user.model.ts
+++ module17/src/model/user.model.ts
@@ -2,17 +2,32 @@ import { Schema, model } from "mongoose";
 import { IUser } from "../interface/user.interface";
 
 const userSchema = new Schema<IUser>({
-    name: { type: String, required: true },
-    email: { type: String, trim: true, lowercase: true, unique: true, required: true },
+    name: { 
+        type: String, 
+        required: [true, "Name is required"] 
+    },
+    email: { 
+        type: String, 
+        trim: true, 
+        lowercase: true, 
+        unique: [true, "Email already exists"], 
+        required: [true, "Email is required"] 
+    },
     role: {
         type: String,
-        enum: ["student", "it", "teacher", "admin"],
+        enum: {
+            values: ["student", "it", "teacher", "admin"],
+            message: "{VALUE} is not a valid role"
+        },
         default: "student"
     },
-    isActive: { type: Boolean, required: true, default: true }
-});
+    isActive: { 
+        type: Boolean, 
+        required: [true, "Status is required"], 
+        default: true 
+    }
+}, { timestamps: true }); // optional: automatically adds createdAt and updatedAt
 
-
-const User = model("User", userSchema)
+const User = model("User", userSchema);
 
 export default User;
